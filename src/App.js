6c94056d562da1ff38580421424787b66ff95d58
petercreations <hello@petercreations.com>
2021-03-15 12:36:38 +0100

    fist draft

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Buttons } from 'Buttons';
+import './index.css';
+
+export const App = () => {
+  return (
+    <div className="btn-wrapper">
+    <Buttons title="Subscribe" 
+    disabled="" />
+    </div>
+  )
+}
